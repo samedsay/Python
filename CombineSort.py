74bdb53382a61c43bcefd52ea062f8d59bbe8880
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-05-28 03:27:42 +0200

    Create CombineSort.py

---
+++ CombineSort.py
@@ -0,0 +1,8 @@
+# Write a function named combine_sort that has two parameters named lst1 and lst2.
+# The function should combine these two lists into one new list and sort the result. Return the new sorted list.
+
+def combine_sort(lst1, lst2):
+  newList = sorted(lst1 + lst2)
+  return newList
+
+print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
