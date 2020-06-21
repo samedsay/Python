5bec8594f5e476751709f300d7322cda72735d9f
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-06-21 20:01:30 +0200

    Update 011Strings.py

--- 011Strings.py
+++ 011Strings.py
@@ -93,3 +93,12 @@ def poem_title_card(poet, title):
   return "The poem \"{}\" is written by {}.".format(title,poet)
 
 ###########################################
+
+def poem_description(publishing_date, author, title, original_work):
+  poem_desc = "The poem {title} by {author} was originally published in {original_work} in {publishing_date}.".format(title=title, author=author, original_work=original_work, publishing_date=publishing_date)
+  return poem_desc
+my_beard_description = poem_description("1974", "Shel Silverstein","My Beard","Where the Sidewalk Ends")
+
+###########################################
+
+
