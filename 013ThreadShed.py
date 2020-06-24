652ea0662cde70e366631b2dd0b0f7b8afa35af4
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-06-24 13:08:35 +0200

    Update and rename 013Thread_Shed.py to 013ThreadShed.py

--- 013ThreadShed.py
+++ 013ThreadShed.py
@@ -113,7 +113,6 @@ green&white;,;09/15/17,   Gail Phelps   ;,;$30.52
 ;,;   $22.66   ;,; green&white&blue;,;09/15/17"""
 
 #------------------------------------------------
-# Start coding below!
 
 daily_sales_replaced = daily_sales.replace(";,;","@")
 print(daily_sales_replaced)
