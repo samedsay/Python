8949c4468a305c991bd53d192897c8c607d4464b
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-06-25 09:28:15 +0200

    Create 017Dictionary.py

---
+++ 017Dictionary.py
@@ -0,0 +1,14 @@
+sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry":22}
+num_cameras = {"backyard": 6, "garage": 2, "driveway": 1}
+
+print(sensors)
+print(num_cameras)
+
+# Create a new dictionary and adding new values.
+
+animals_in_zoo = {}
+animals_in_zoo["zebras"] = 8
+animals_in_zoo["monkeys"] = 12
+animals_in_zoo["dinosaurs"] = 0
+
+print(animals_in_zoo)
