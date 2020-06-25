c754df6002f3e10a7d1ff0ceabfbfdc51e0d2298
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-06-25 09:36:06 +0200

    Update 017Dictionary.py

--- 017Dictionary.py
+++ 017Dictionary.py
@@ -12,3 +12,18 @@ animals_in_zoo["monkeys"] = 12
 animals_in_zoo["dinosaurs"] = 0
 
 print(animals_in_zoo)
+
+# Adding more than one values at a time
+
+user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
+
+user_ids.update({"theLooper": 138475, "stringQueen": 85739})
+
+print(user_ids)
+
+# Overwrite the value 
+
+oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck", "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
+
+oscar_winners["Supporting Actress"] = "Viola Davis"
+oscar_winners["Best Picture"] = "Moonlight"
