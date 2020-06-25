9ecbf5cebf9c6a1e5e7d8ccabb57e1a5c2939787
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-06-25 09:43:31 +0200

    Update 017Dictionary.py

--- 017Dictionary.py
+++ 017Dictionary.py
@@ -5,7 +5,6 @@ print(sensors)
 print(num_cameras)
 
 # Create a new dictionary and adding new values.
-
 animals_in_zoo = {}
 animals_in_zoo["zebras"] = 8
 animals_in_zoo["monkeys"] = 12
@@ -14,7 +13,6 @@ animals_in_zoo["dinosaurs"] = 0
 print(animals_in_zoo)
 
 # Adding more than one values at a time
-
 user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
 
 user_ids.update({"theLooper": 138475, "stringQueen": 85739})
@@ -22,8 +20,21 @@ user_ids.update({"theLooper": 138475, "stringQueen": 85739})
 print(user_ids)
 
 # Overwrite the value 
-
 oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck", "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
 
 oscar_winners["Supporting Actress"] = "Viola Davis"
 oscar_winners["Best Picture"] = "Moonlight"
+
+
+# Zip method to unify two different list in to Dictionary
+drinks = ["espresso", "chai", "decaf", "drip"]
+caffeine = [64, 40, 0, 120]
+
+zipped_drinks =zip(drinks, caffeine)
+
+print(zipped_drinks)
+drinks_to_caffeine = {key:value for key, value in zipped_drinks}
+print(drinks_to_caffeine)
+
+drinks_to_caffeine2 = {key:value for key, value in zip(drinks, caffeine)}
+print(drinks_to_caffeine2)
