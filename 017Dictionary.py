f50398f9eaeeb58af08c1bf634ac1bd43664d990
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-06-25 11:08:37 +0200

    Update 017Dictionary.py

--- 017Dictionary.py
+++ 017Dictionary.py
@@ -93,6 +93,17 @@ pct_women_in_occupation = {"CEO": 28, "Engineering Manager": 9, "Pharmacist": 58
 
 for key,value in pct_women_in_occupation.items():
   print("Women make up " + str(value) + " percent of " + key + "s")
+  
+# Small Tarot Game
+tarot = { 1:	"The Magician", 2:	"The High Priestess", 3:	"The Empress", 4:	"The Emperor", 5:	"The Hierophant", 6:	"The Lovers", 7:	"The Chariot", 8:	"Strength", 9:	"The Hermit", 10:	"Wheel of Fortune", 11:	"Justice", 12:	"The Hanged Man", 13:	"Death", 14:	"Temperance", 15:	"The Devil", 16:	"The Tower", 17:	"The Star", 18:	"The Moon", 19:	"The Sun", 20:	"Judgement", 21:	"The World", 22: "The Fool"}
+
+spread = {}
+spread["past"] = tarot.pop(13)
+spread["present"] = tarot.pop(22)
+spread["future"] = tarot.pop(10)
+
+for key, value in spread.items():
+  print("Your {} is the {} card.".format(key,value))
 
 
 
