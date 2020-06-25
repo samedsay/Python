87d6d5e666c5931d253b09b8932cab7ebc0338a3
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-06-25 10:10:12 +0200

    Update 017Dictionary.py

--- 017Dictionary.py
+++ 017Dictionary.py
@@ -38,3 +38,16 @@ print(drinks_to_caffeine)
 
 drinks_to_caffeine2 = {key:value for key, value in zip(drinks, caffeine)}
 print(drinks_to_caffeine2)
+
+# Playlist Example
+songs = ["Like a Rolling Stone", "Satisfaction", "Imagine", "What's Going On", "Respect", "Good Vibrations"]
+playcounts = [78, 29, 44, 21, 89, 5]
+
+plays = {key:value for key, value in zip(songs,playcounts)}
+
+print(plays)
+plays["Purple Haze"] = 1
+plays["Respect"] = 94
+library = {"The Best Songs":plays, "Sunday Feelings":{}}
+print(library)
+
