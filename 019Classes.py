fb9dc30f3c474660034651748ec8b4cb6f6ca4e2
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-06-26 12:52:43 +0200

    Update 019Classes.py

--- 019Classes.py
+++ 019Classes.py
@@ -3,3 +3,57 @@ how_many_s = [{'s': False}, "sassafrass", 18, ["a", "c", "s", "d", "s"]]
 for element in how_many_s:
   if hasattr(element, "count"):
     print(element.count("s"))
+    
+# # # # # # # # #
+    
+class Circle:
+  pi = 3.14
+  def __init__(self, diameter):
+    print("Creating circle with diameter {d}".format(d=diameter))
+    # Add assignment for self.radius here:
+    self.radius = diameter/2
+  def circumference(self):
+    return 2 * self.pi * self.radius
+  
+medium_pizza = Circle(12)
+teaching_table = Circle(36)
+round_room = Circle(11460)
+
+print(medium_pizza.circumference())
+print(teaching_table.circumference())
+print(round_room.circumference())
+
+### dir() method on objects
+print(dir(5))
+
+def this_function_is_an_object():
+  pass
+
+print(dir(this_function_is_an_object))
+
+### __repr__(self) method takes no parameter and return only string
+
+class Circle:
+  pi = 3.14
+  
+  def __init__(self, diameter):
+    self.radius = diameter / 2
+  
+  def area(self):
+    return self.pi * self.radius ** 2
+  
+  def circumference(self):
+    return self.pi * 2 * self.radius
+
+  def __repr__(self):
+    return "Circle with radius {radius}".format(radius=self.radius)
+  
+  
+medium_pizza = Circle(12)
+teaching_table = Circle(36)
+round_room = Circle(11460)
+
+print(medium_pizza)
+print(teaching_table)
+print(round_room)
+
