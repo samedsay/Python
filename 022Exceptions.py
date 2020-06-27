955bb2a3db1bbd644319a11037ecef3ee34c7372
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-06-27 08:25:59 +0200

    Create 022Exceptions.py

---
+++ 022Exceptions.py
@@ -0,0 +1,16 @@
+# Defining my exception
+class OutOfStock(Exception):
+  pass
+
+class CandleShop:
+  name = "Here's a Hot Tip: Buy Drip Candles"
+  def __init__(self, stock):
+    self.stock = stock
+    
+  def buy(self, color):
+    if self.stock[color] < 1:
+      raise OutOfStock
+    self.stock[color] = self.stock[color] - 1
+
+candle_shop = CandleShop({'blue': 6, 'red': 2, 'green': 0})
+candle_shop.buy('blue')
