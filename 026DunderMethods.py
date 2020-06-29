60900a3ae9ea222e4cdbb5c03979a8b5b0a4307a
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-06-29 23:15:43 +0200

    Update 026DunderMethods.py

--- 026DunderMethods.py
+++ 026DunderMethods.py
@@ -15,3 +15,15 @@ chlorine = Atom("Cl")
 salt = Molecule([sodium, chlorine])
 # salt = sodium + chlorine
 
+###########################################
+
+class LawFirm:
+  def __init__(self, practice, lawyers):
+    self.practice = practice
+    self.lawyers = lawyers
+  def __len__(self):
+    return len(self.lawyers)
+  def __contains__(self,lawyer):
+    return lawyer in self.lawyers
+    
+d_and_p = LawFirm("Injury", ["Donelli", "Paderewski"])
