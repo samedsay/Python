6b36c094d9e1ea2c2744e16b8a8056ed7d002681
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-07-01 19:43:49 +0200

    Update 028MixFunctionExample.py

--- 028MixFunctionExample.py
+++ 028MixFunctionExample.py
@@ -1,8 +1,21 @@
+def unique_english_letters(word):
+  letterList = []
+  for letter in word:
+    if letter not in letterList:
+      letterList.append(letter)
+  return len(letterList)
+
+
+def count_char_x(word, x):
+  return word.count(x)
+
+
 def substring_between_letters(word, start, end):
   if word.find(start) == -1 or word.find(end) == -1:
     return word
   else:
     return word[word.find(start)+1:word.find(end)]
+    
     
 def x_length_words(sentence, x):
   sentenceArray = sentence.split()
@@ -10,6 +23,7 @@ def x_length_words(sentence, x):
     if len(word) < x:
       return False
   return True
+
   
 def check_for_name(sentence, name):
   if name.lower() in sentence.lower():
@@ -17,8 +31,30 @@ def check_for_name(sentence, name):
   else:
     return False
     
+    
 def every_other_letter(word):
   every_other = ""
   for i in range(0, len(word), 2):
     every_other += word[i]
   return every_other
+
+
+def reverse_string(word):
+  return word[::-1]
+
+
+def make_spoonerism(word1, word2):
+  firstLetterWord1 = word1[0]
+  word1 = word2[:1] + word1[1:]
+  word2 = firstLetterWord1 + word2[1:]
+  return word1 + " " + word2
+
+
+def add_exclamation(word):
+  if len(word) >= 20:
+    return word
+  else:
+    while len(word) < 20:
+      word += "!"
+    return word
+
