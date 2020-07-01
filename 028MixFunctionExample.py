1227c55f48eac67bfd41956c4f835c86a480480c
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-07-01 19:54:54 +0200

    Update 028MixFunctionExample.py

--- 028MixFunctionExample.py
+++ 028MixFunctionExample.py
@@ -5,17 +5,20 @@ def unique_english_letters(word):
       letterList.append(letter)
   return len(letterList)
 
+######################################################
 
 def count_char_x(word, x):
   return word.count(x)
 
+######################################################
 
 def substring_between_letters(word, start, end):
   if word.find(start) == -1 or word.find(end) == -1:
     return word
   else:
     return word[word.find(start)+1:word.find(end)]
-    
+
+######################################################  
     
 def x_length_words(sentence, x):
   sentenceArray = sentence.split()
@@ -24,13 +27,15 @@ def x_length_words(sentence, x):
       return False
   return True
 
+######################################################
   
 def check_for_name(sentence, name):
   if name.lower() in sentence.lower():
     return True
   else:
     return False
-    
+ 
+######################################################
     
 def every_other_letter(word):
   every_other = ""
@@ -38,10 +43,12 @@ def every_other_letter(word):
     every_other += word[i]
   return every_other
 
+######################################################
 
 def reverse_string(word):
   return word[::-1]
 
+######################################################
 
 def make_spoonerism(word1, word2):
   firstLetterWord1 = word1[0]
@@ -49,6 +56,7 @@ def make_spoonerism(word1, word2):
   word2 = firstLetterWord1 + word2[1:]
   return word1 + " " + word2
 
+######################################################
 
 def add_exclamation(word):
   if len(word) >= 20:
@@ -57,4 +65,16 @@ def add_exclamation(word):
     while len(word) < 20:
       word += "!"
     return word
+
+######################################################
+  
+def sum_values(my_dictionary):
+  total = 0
+  for value in my_dictionary.values():
+    total += value
+  return total
+
+######################################################
+
+
 
