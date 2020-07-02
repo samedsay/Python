c23d5c8e7c5571f4777fa60d6389a178f9586be9
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-07-02 14:54:18 +0200

    Update 029FileOperations.py

--- 029FileOperations.py
+++ 029FileOperations.py
@@ -12,3 +12,16 @@ with open("how_many_lines.txt") as lines_doc:
 with open("just_the_first.txt") as first_line_doc:
   first_line = first_line_doc.readline()
 print(first_line)
+
+# WRITING A FILE
+with open("bad_bands.txt", "w") as bad_bands_doc:
+  bad_bands_doc.write("4Me")
+  
+# APPENDING TO A FILE
+with open("cool_dogs.txt", "a") as cool_dogs_file:
+  cool_dogs_file.write("Air Buddy")
+  
+# READING A CSV FILE
+import csv
+with open("cool_csv.csv") as cool_csv_file:
+  cool_csv_dict = csv.DictReader(cool_csv_file)
