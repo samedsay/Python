5e2ebb3cd1cf186112908309193c6ccd9e1c07a9
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-07-02 14:20:37 +0200

    Update 029FileOperations.py

--- 029FileOperations.py
+++ 029FileOperations.py
@@ -1,4 +1,14 @@
-# READ FILE
+# READING A FILE
 with open("welcome.txt") as text_file:
   text_data = text_file.read()
 print(text_data)
+
+# READING LINES
+with open("how_many_lines.txt") as lines_doc:
+  for line in lines_doc.readlines():
+    print(line)
+    
+# READING A LINE
+with open("just_the_first.txt") as first_line_doc:
+  first_line = first_line_doc.readline()
+print(first_line)
