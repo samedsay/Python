561fd655ccc35386a242b510a6274b31f164c4e5
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-07-09 13:46:38 +0200

    Update 035NodeIntro.py

--- 035NodeIntro.py
+++ 035NodeIntro.py
@@ -9,3 +9,23 @@ class Node:
     
   def get_link_node(self):
     return self.link_node
+  
+  # Setters
+  def set_link_node(self, link_node):
+    self.link_node = link_node
+    
+yacko = Node("likes to yak")
+wacko = Node("has a penchant for hoarding snacks")
+dot = Node("enjoys spending time in movie lots") 
+
+dot.set_link_node(wacko)
+yacko.set_link_node(dot) 
+
+dots_data = yacko.get_link_node().get_value()
+wackos_data = dot.get_link_node().get_value() 
+
+print(dots_data)
+print(wackos_data)
+
+
+
