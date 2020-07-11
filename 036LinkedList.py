04cb3769e5dd99c7adc9052f1ae00635e1a9abbb
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-07-11 11:44:27 +0200

    Create 036LinkedList.py

---
+++ 036LinkedList.py
@@ -0,0 +1,21 @@
+# Node class
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+    
+  def get_value(self):
+    return self.value
+  
+  def get_next_node(self):
+    return self.next_node
+  
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+
+# LinkedList clas
+class LinkedList:
+  def __init__(self,value = None):
+    self.head_node = Node(value)
+  def get_head_node(self):
+    return self.head_node
